a10132ec3dbdd3cd5ecee0f0f8250c94f26bc691
creatio <creatio.x.14@gmail.com>
2011-06-05 10:34:34 +0200

    changed file naming

---
+++ problems/euler_004.d
@@ -0,0 +1,15 @@
+/**
+ * euler_004.d
+ * ```````````````````````````````````````````````````````````````````````````
+ * A palindromic number reads the same both ways. The largest palindrome made 
+ * from the product of two 2-digit numbers is 9009 = 91 x 99.
+ * Find the largest palindrome made from the product of two 3-digit numbers.
+ * ```````````````````````````````````````````````````````````````````````````
+ */
+module euler_004;
+import std.stdio;
+
+int main(string[] argv)
+{
+	return 0;
+}
